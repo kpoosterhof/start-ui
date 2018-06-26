5b47346b2aff596ba88cb1c27673d33c4e745e6b
kp <16428428+kpoosterhof@users.noreply.github.com>
2018-06-26 12:24:23 +0200

    reverse

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,14 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) {}
+
+  getMovie() {
+    return this.http.get('/server/');
+  }
+}
