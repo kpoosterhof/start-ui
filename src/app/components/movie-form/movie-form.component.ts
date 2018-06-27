9a2933cb23e2cd04e91b13cb03452480cf96c9e2
kpoosterhof <kp.oosterhof@gmail.com>
2018-06-27 12:35:36 +0200

    playing around with validators

--- src/app/components/movie-form/movie-form.component.ts
+++ src/app/components/movie-form/movie-form.component.ts
@@ -1,44 +1,49 @@
-import {MovieService} from '../../services/movie.service';
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { throwError } from 'rxjs';
 
-@Component({
-  selector: 'app-movie-form',
-  templateUrl: './movie-form.component.html',
-  styleUrls: ['./movie-form.component.css']
-})
+@Component( {
+    selector: 'app-movie-form',
+    templateUrl: './movie-form.component.html',
+    styleUrls: ['./movie-form.component.css']
+} )
 export class MovieFormComponent implements OnInit {
-  movieform: FormGroup;
-  validMessage: string = "";
+    movieform: FormGroup;
+    validMessage: string = "";
 
-  constructor(private movieService: MovieService) {}
+    constructor( private movieService: MovieService ) { }
 
-  ngOnInit() {
-    this.movieform = new FormGroup({
-      title: new FormControl('', Validators.required),
-      director: new FormControl('', Validators.required),
-      genre: new FormControl('', Validators.required),
-      yearOfRelease: new FormControl('', Validators.required)
-    });
-  }
-  
-  addMovie() {
-
-    if (this.movieform.valid) {
-      this.validMessage = 'The movie has been added.';
-      this.movieService.addMovie(this.movieform.value).subscribe(
-        data => {
-          this.movieform.reset();
-          return true;
-        },
-        error => {
-          return throwError(new Error(error));
-        }
-      );
-    } else {
-      this.validMessage = 'Please fill out the form before submitting.';
+    ngOnInit() {
+        this.movieform = new FormGroup( {
+            title: new FormControl( '', { validators: [Validators.required, Validators.maxLength( 255 )] } ),
+            director: new FormControl( '', { validators: [Validators.required, Validators.maxLength( 255 ), Validators.pattern( '^([^0-9]*)$' )] } ),
+            genre: new FormControl( '', { validators: [Validators.required, Validators.maxLength( 255 )] } ),
+            yearOfRelease: new FormControl( '', { validators: [Validators.required, Validators.min( 1878 ), Validators.max( this.getCurrentYear() )] } )
+        } );
     }
-  }
+
+    addMovie() {
+
+        if ( this.movieform.valid ) {
+            this.validMessage = 'The movie has been added.';
+            this.movieService.addMovie( this.movieform.value ).subscribe(
+                data => {
+                    this.movieform.reset();
+                    return true;
+                },
+                error => {
+                    return throwError( new Error( error ) );
+                }
+            );
+        } else {
+            this.validMessage = 'Please fill out the form before submitting.';
+        }
+    }
+
+    getCurrentYear() {
+        let date = new Date();
+        return date.getFullYear();
+    }
 
 }
